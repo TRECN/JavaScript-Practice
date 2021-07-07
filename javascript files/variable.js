13e24af5fccb3fa3160f7fa32bd4baf7f03837d7
TRECN <rishabh1103@outlook.com>
2021-07-07 21:49:05 +0530

    commit

--- javascript files/variable.js
+++ javascript files/variable.js
@@ -2,6 +2,8 @@ var x = 5;
 var y = 6;
 var z = x + y;
 document.getElementById('d').innerHTML="The value is: "+z;
-
-var a=9,k="Rishabh",m="hi";
+//we can declare it in this way too
+var a=9,
+k="Rishabh",
+m="hi";
 document.getElementById('e').innerHTML=m+k+a;
