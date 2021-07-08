ad9514401861d659963a6d63647e07e9d8e45dfe
TRECN <rishabh1103@outlook.com>
2021-07-08 22:11:00 +0530

    changes object js

--- javascript files/objects.js
+++ javascript files/objects.js
@@ -1,2 +1,14 @@
+//syntax
+//const name={type:data,....};
+//instead of const we can also use let and var
 const car={type:"ciaz", model:"100vz", color:"green"};
 document.getElementById('a').innerHTML="the car type is "+car.type;
+//--------------
+// Create an object:
+const person = {
+    firstName: "John",
+    lastName: "Doe",
+    age: 50,
+    eyeColor: "blue"
+  };
+  document.getElementById('b').innerHTML=person.firstName + " is " + person.age + " years old.";
