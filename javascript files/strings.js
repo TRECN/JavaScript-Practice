8c501114d4a971eb97298f5066d90f6905d63a08
TRECN <rishabh1103@outlook.com>
2021-07-09 22:02:36 +0530

    changes string js

--- javascript files/strings.js
+++ javascript files/strings.js
@@ -7,6 +7,10 @@ document.getElementById('a').innerHTML=text;
 //variable substitutiuon
 let firstName="John";
 let lastName = "Doe";
-
 let text1=`welcome ${firstName} ${lastName}`;
 document.getElementById('b').innerHTML=text1;
+//expression Substitution
+let price=10;
+let vat=0.25;
+let total = `Total: ${(price * (1 + vat)).toFixed(2)}`;
+document.getElementById('c').innerHTML=total;
