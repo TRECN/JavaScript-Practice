773e3dd25ee936eae3452ee9b5f24cfe1cd59334
TRECN <rishabh1103@outlook.com>
2021-07-09 21:54:56 +0530

    changes string js

--- javascript files/strings.js
+++ javascript files/strings.js
@@ -4,3 +4,9 @@ brown fox jumps
  over the lazy 
  dog.`;
 document.getElementById('a').innerHTML=text;
+//variable substitutiuon
+let firstName="John";
+let lastName = "Doe";
+
+let text1=`welcome ${firstName} ${lastName}`;
+document.getElementById('b').innerHTML=text1;
