1edd3ef006afeb3d3b41ccdd4d30e8b9ee16ab49
TRECN <rishabh1103@outlook.com>
2021-07-10 13:00:25 +0530

    changes array js

--- javascript files/array.js
+++ javascript files/array.js
@@ -1,3 +1,13 @@
 //Array example
-const cars=["Saab","Volvo","BMW"];
+const cars=[
+    "Saab",
+    "Volvo",
+    "BMW"
+];
 document.getElementById('a').innerHTML=cars;
+//another syntax
+const cars2=[];
+cars2[0]="saab";
+cars2[1]="Volvo";
+cars2[2]="bmw";
+document.getElementById('b').innerHTML=cars2;
