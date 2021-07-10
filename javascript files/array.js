af0e514ca6e23b9ac0ac8d84f6dcdfc84acad9f7
TRECN <rishabh1103@outlook.com>
2021-07-10 13:30:52 +0530

    changes array js

--- javascript files/array.js
+++ javascript files/array.js
@@ -5,20 +5,24 @@ const cars=[
     "BMW"
 ];
 document.getElementById('a').innerHTML=cars;
+
 //another syntax
 const cars2=[];
 cars2[0]="saab";
 cars2[1]="Volvo";
 cars2[2]="bmw";
 document.getElementById('b').innerHTML=cars2;
+
 //using new keyword
 const cars3 = new Array ("Saab","Volvo","BMW");
 document.getElementById('c').innerHTML=cars3;
+
 //accessing array elements and changing an arry element
 const cars4 = ["Saab", "Volvo", "BMW"];
 let x = cars[0]; 
 cars[0]="ford";
 document.getElementById('d').innerHTML="changed "+x+"<br>the array: "+cars4;
+
 //looping an array
 const fruits = ["Banana", "Orange", "Apple", "Mango"];
 let fln=fruits.length;
@@ -28,3 +32,12 @@ for (let i = 0; i < fln; i++) {
   }
   text += "</ul>";
 document.getElementById('e').innerHTML=text;
+//you can also use array.forEach()
+let text2="<ul>";
+fruits.forEach(myFunction);
+text2+="</ul>";
+document.getElementById('f').innerHTML=text2;
+
+function myFunction(value){
+    text2+="<li>"+value+"</li>";
+}
