f9348f00f8056d2745b688f272b044aa2f5318cc
TRECN <rishabh1103@outlook.com>
2021-07-10 13:04:59 +0530

    changes array js

--- javascript files/array.js
+++ javascript files/array.js
@@ -11,3 +11,6 @@ cars2[0]="saab";
 cars2[1]="Volvo";
 cars2[2]="bmw";
 document.getElementById('b').innerHTML=cars2;
+//using new keyword
+const cars3 = new Array ("Saab","Volvo","BMW");
+document.getElementById('c').innerHTML=cars3;
