2bb6659dc0a17459f5dc17a539e23ee2ba779971
TRECN <rishabh1103@outlook.com>
2021-07-11 16:26:48 +0530

    changes arraymethods js

--- javascript files/arraymethods.js
+++ javascript files/arraymethods.js
@@ -1,10 +1,20 @@
 //using array methods
+
 //toString
 const fruits=["Banana", "Orange", "Apple", "Mango"];
 document.getElementById('a').innerHTML=fruits.toString();
+
 //join
 document.getElementById('b').innerHTML=fruits.join('*');
+
 //pop
 document.getElementById('c').innerHTML=fruits;
 fruits.pop();
 document.getElementById('d').innerHTML=fruits;
+
+//push
+document.getElementById('e').innerHTML=fruits;
+function myfunction(){
+    fruits.push("kiwi");
+    document.getElementById('e').innerHTML=fruits;
+}
