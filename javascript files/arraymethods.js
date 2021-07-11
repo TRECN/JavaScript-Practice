5b87550064d927fffb7bb85db522c356b33e3682
TRECN <rishabh1103@outlook.com>
2021-07-11 16:17:52 +0530

    changes arraymethods js

--- javascript files/arraymethods.js
+++ javascript files/arraymethods.js
@@ -1,3 +1,6 @@
 //using array methods
+//toString
 const fruits=["Banana", "Orange", "Apple", "Mango"];
 document.getElementById('a').innerHTML=fruits.toString();
+//join
+document.getElementById('b').innerHTML=fruits.join('*');
