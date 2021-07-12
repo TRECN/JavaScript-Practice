1827274f8eeea377f2c6bc378289ed3197b0627d
TRECN <rishabh1103@outlook.com>
2021-07-12 16:05:10 +0530

    changes es6 js

---
+++ javascript files/javascriptes6.js
@@ -0,0 +1,11 @@
+let x=10;
+//here x=10
+    {
+        let x=2;
+        //here x=2
+        document.getElementById('a').innerHTML=x;
+    }
+//here x=10
+document.getElementById('b').innerHTML=x;
+
+
