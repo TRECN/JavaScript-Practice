30df9fff68e8f89f632e9faf17e3274a8a39f90a
TRECN <rishabh1103@outlook.com>
2021-07-13 19:32:57 +0530

    changes es6-2 js

--- javascript files/es6-2.js
+++ javascript files/es6-2.js
@@ -11,3 +11,12 @@ fruit.set(bananas,300);
 fruit.set(oranges,200);
 //display object type 
 document.getElementById('a').innerHTML=fruit;
+//showing the value for a key in a map object
+document.getElementById('b').innerHTML=fruit.get(bananas);
+//another way to create a map
+const fruits=new Map([
+    [apples,500],
+    [bananas,300],
+    [oranges,200]
+]);
+document.getElementById('c').innerHTML=fruits;
