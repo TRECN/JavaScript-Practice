f6d72e26306cd3f60961d646441cbe14f7e00248
TRECN <rishabh1103@outlook.com>
2021-07-14 17:44:05 +0530

    changes classes html

--- javascript files/classes.js
+++ javascript files/classes.js
@@ -8,3 +8,17 @@ class car{
 }
 const myCar=new car("ford",64);
 document.getElementById('b').innerHTML=myCar.name;
+
+class cars{
+    constructor(name, year){
+        this.name=name;
+        this .year=year;
+    }
+    age(){
+        let date = new Date();
+        return date.getFullYear()-this.year;
+
+    }
+}
+let myCars=new cars("ford",2014);
+document.getElementById('c').innerHTML=myCars.age();
